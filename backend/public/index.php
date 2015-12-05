581a22451f61601603676e44f80b87105a921eac
Rostislav Raykov <z@zbrox.org>
2015-12-05 20:54:06 +0100

    Add simple user manipulation + login/registration API endpoints

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,10 +5,61 @@ require '../config.php';
 $app = new \Slim\Slim();
 $app->view(new \JsonApiView());
 $app->add(new \JsonApiMiddleware());
+// $app->add(new \Slim\Middleware\JwtAuthentication([
+//     "secret" => $appConfig['jwtSecret']
+// ]));
+
+// auth
+$app->post($appConfig['apiPrefix'] . '/login', function() use ($app) {
+    $password_hash = password_hash($app->request->post('password'), PASSWORD_DEFAULT);
+    $user = User::whereEmail($app->request->post('email'))->first();
+    if ($password_hash == $user->password_hash) {
+        $app->render(200);
+    } else {
+        $app->render(403);
+    }
+});
+
+$app->post($appConfig['apiPrefix'] . '/register', function() use ($app) {
+    $user = new User();
+    $user->email = $app->request->post('email');
+    $user->password_hash = password_hash($app->request->post('password'), PASSWORD_DEFAULT);
+
+    if ($user->save()) {
+        $app->render(200);
+    } else {
+        $app->render(500);
+    }
+});
 
 // users
 $app->get($appConfig['apiPrefix'] . '/users', function () use ($app) {
     $app->render(200, ['result' => User::all()]);
+});
+
+$app->get($appConfig['apiPrefix'] . '/users/:id', function ($id) use ($app) {
+    $user = User::find($id);
+    if ($user) {
+        $app->render(200, ['result' => $user]);
+    } else {
+        $app->render(404);
+    }
+});
+
+$app->put($appConfig['apiPrefix'] . '/users/:id', function ($id) use ($app) {
+    $user = User::find($id);
+
+    $valid = $user->validate($app->request->post());
+    if (!$valid) {
+        $app->render(400, ['validation' => $user->errors]);
+    }
+
+    $user->fill($app->request->post());
+    if ($user->save()) {
+        $app->render(200);
+    } else {
+        $app->render(500);
+    }
 });
 
 // orders
