5a3d77af7cd683cd9893ddc26700529bf9871995
Rostislav Raykov <z@zbrox.org>
2015-12-05 17:31:30 +0100

    Fill in the allowed properties for an order to use with mass assignment

--- backend/models/Order.php
+++ backend/models/Order.php
@@ -3,6 +3,8 @@ require '../vendor/autoload.php';
 
 class Order extends \Illuminate\Database\Eloquent\Model
 {
+	protected $fillable = array('user_id', 'comment');
+
 	public function products()
     {
         return $this->belongsToMany('\Product');
