ee0711d5296b1ee11b8db4cae7a81b7d710f3b93
Rostislav Raykov <z@zbrox.org>
2015-12-06 12:23:13 +0100

    Attach the jwt to the app object

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,7 +7,10 @@ $app->view(new \JsonApiView());
 $app->add(new \JsonApiMiddleware());
 $app->add(new \Slim\Middleware\JwtAuthentication([
     'secret'   => $appConfig['jwtSecret'],
-    'path'     => '/api'
+    'path'     => '/api',
+    'callback' => function ($options) use ($app) {
+        $app->jwt = $options['decoded'];
+    }
 ]));
 
 require '../routes/users.php';
