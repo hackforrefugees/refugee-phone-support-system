cbfe336c675f501c173918dde557dd40d0b8bdb1
Rostislav Raykov <z@zbrox.org>
2015-12-06 16:00:01 +0100

    Attach current user and jwt token to rootscope and redirect to users list

--- frontend/app/js/login-ctrl.js
+++ frontend/app/js/login-ctrl.js
@@ -7,8 +7,12 @@ module.exports = function ($rootScope, $timeout, $state, $stateParams, User) {
 	this.login = details => {
 		this.loggingIn = true;
 
-		User.authenticate(details, function (result) {
-			$rootScope.currentUser = result;
+		User.login(details, function (result) {
+			$rootScope.currentUser = result.user;
+			$rootScope.jwt = result.token;
+			$state.go('user.list');
+		}, function(error) {
+			console.log('error logging in');
 		});
 	};
 };
